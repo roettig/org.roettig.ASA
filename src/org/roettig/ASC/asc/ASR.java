90b0a1b698ac6757611df673512f8540b386e856
Marc Roettig <marc.roettig@googlemail.com>
2011-08-16 16:44:31 +0200

    added ImportanceRanking to ASR pipeline.

--- src/org/roettig/ASC/asc/ASR.java
+++ src/org/roettig/ASC/asc/ASR.java
@@ -2,18 +2,30 @@ package org.roettig.ASC.asc;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.biojava.bio.seq.Sequence;
+import org.roettig.ASC.kernels.BLOSUMStringKernel;
+import org.roettig.ASC.kernels.IndexedStringKernel;
 import org.roettig.MLToolbox.base.Prediction;
 import org.roettig.MLToolbox.base.impl.DefaultInstanceContainer;
+import org.roettig.MLToolbox.base.instance.DualInstance;
 import org.roettig.MLToolbox.base.instance.InstanceContainer;
 import org.roettig.MLToolbox.base.instance.PrimalInstance;
+import org.roettig.MLToolbox.base.label.FactorLabel;
 import org.roettig.MLToolbox.base.label.Label;
 import org.roettig.MLToolbox.base.label.NumericLabel;
 import org.roettig.MLToolbox.kernels.RBFKernel;
+import org.roettig.MLToolbox.model.CSVCModel;
 import org.roettig.MLToolbox.model.NuSVRModel;
 import org.roettig.MLToolbox.model.SelectedModel;
+import org.roettig.MLToolbox.validation.GMeasure;
 import org.roettig.MLToolbox.validation.ModelValidation;
 import org.roettig.PDBTools.ResidueLocator;
 import org.roettig.SequenceTools.SeqTools;
@@ -72,12 +84,91 @@ public class ASR extends ASAFlow
 			idx++;
 		}
 	}
-	
-	
-
+		
 	@Override
 	protected void analyzeImportance()
-	{
+	{		
+		final class PositionImportance
+		{
+			public PositionImportance(int _pos, double _imp)
+			{
+				pos = _pos; imp = _imp;  
+			}
+
+			public boolean equals(Object o)
+			{
+				if (!(o instanceof PositionImportance))
+					return false;
+
+				PositionImportance p = (PositionImportance) o;
+				return pos==p.pos;
+			}
+
+			public int hashCode()
+			{
+				return pos;
+			}
+
+
+			public int pos;
+			public double imp;
+		}
+
+		
+
+		Logger ml2logger = Logger.getLogger("org.roettig.MLToolbox.validation.ModelValidation");
+		Level old_lvl = ml2logger.getLevel();
+		ml2logger.setLevel(Level.OFF);
+
+
+		int K = ctx.ASRidx.size();
+
+		double score[] = new double[K];
+		
+		Vector<PositionImportance> imps = new Vector<PositionImportance>();
+		
+		for(int k=0;k<K;k++)
+		{
+			IndexedStringKernel f = new IndexedStringKernel( new BLOSUMStringKernel() );
+			f.addFixedIndex(k);
+
+
+			NuSVRModel<DualInstance<String>> m = new NuSVRModel<DualInstance<String>>(f);
+			
+			m.setNU(0.3);
+			m.setC(100.0);
+
+			double qual = 0.0;
+			try
+			{
+				qual = ModelValidation.CV(2, ctx.stringsamples, m);
+			} 
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+			PositionImportance imp = new PositionImportance((int)k+1,qual);
+			imps.add(imp);
+		}
+
+		Collections.sort(imps, new Comparator< PositionImportance >() {
+			public int compare(PositionImportance e1, PositionImportance e2) 
+			{
+				Double accu1 = e1.imp;Double accu2 = e2.imp; 
+				return accu1.compareTo(accu2);
+			}});
+
+		log("analyzeImportance","calculating ASC residue importance");
+		for(PositionImportance imp : imps)
+		{
+			log("analyzeImportance",String.format(Locale.ENGLISH,"pos=%2d accu=%.2f",imp.pos,imp.imp));
+			score[imp.pos-1] = imp.imp;
+		}
+		ctx.jd.setScore(0, 1, score);
+		
+		ml2logger.setLevel(old_lvl);
+		
 	}
 
 	@Override
